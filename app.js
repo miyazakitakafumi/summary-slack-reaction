33f659f808d44ac652777b24655b9bbd01693481
宮崎貴史 <>
2020-05-09 11:51:29 +0900

    add eslint and convertUserName

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
 const dayjs = require('dayjs');
-const history = require('./history')
-
-console.log(dayjs('').unix());
+const user = require('./user');
+const history = require('./history');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -15,18 +14,28 @@ app.message('ひとちゃん', async ({ message, say }) => {
 });
 
 app.message('test', async ({ message, say }) => {
+  const userList = await user.getUserList(app);
+
   const channelList = await app.client.conversations.list({
     token: process.env.SLACK_BOT_TOKEN,
-  })
+  });
 
-  const allMessages = await getAllMessages(app, channelList.channels)
-  // console.log(allMessages)
+  const allMessages = await getAllMessages(app, channelList.channels);
+
+  // console.log(allMessages);
 
   const reactions = allMessages.filter(m => m.reactions !== undefined).reduce((prev, current) => {
     return [...prev, ...current.reactions]
   }, [])
 
-  console.log(reactions)
+  const reactionsModified = reactions.map(r => {
+    return {
+      ...r,
+      users: r.users.map(u => user.getUserRealName(userList, u))
+    }
+  })
+
+  console.log(reactionsModified)
 });
 
 (async () => {
@@ -37,13 +46,14 @@ app.message('test', async ({ message, say }) => {
 
 
 const getAllMessages = async (app, channels) => {
-  let result = []
+  let result = [];
+
 
   await Promise.all(channels.map(async c => {
-    const historyPerChannel = await app.client.conversations.history(history.createOption(c.id))
+    const historyPerChannel = await app.client.conversations.history(history.createOption(c.id));
 
     result = [...result, ...historyPerChannel.messages]
-  }))
+  }));
 
   return result
-}
+};
---
+++ user.js
@@ -0,0 +1,18 @@
+const getUserList = async app => {
+  const response = await app.client.users.list({
+    token: process.env.SLACK_BOT_TOKEN,
+  });
+
+  return response.ok ? response.members : [];
+};
+
+const getUserRealName = (userList = [], id) => {
+  const user = userList.find(u => u.id === id);
+  return user === undefined ? id : user.real_name;
+};
+
+
+module.exports = {
+  getUserList,
+  getUserRealName,
+};
